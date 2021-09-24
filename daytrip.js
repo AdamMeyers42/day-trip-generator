9959c2a88b15d737920d462358ccb0157c64ecfd
AdamMeyers42 <adammeyers3551@gmail.com>
2021-09-24 13:05:12 -0700

    Additional Function for DRY, made userInput = TA so while loop initiates

--- daytrip.js
+++ daytrip.js
@@ -6,37 +6,42 @@ let transportations = ['Plane', 'Train', 'Automobiles', 'Bird']
 let entertainments = ['All Night Club', 'Comedy Show', 'Bungee Jumping', 'Relaxing Beach Day']
 
 //Create a function to randomize arrays
-function randomizeArrays(ranArray){
+function randomizeArrays(ranArray) {
     let index = Math.floor(Math.random() * ranArray.length)
     let element = ranArray[index]
-    return element 
+    return element
 }
 
-//Call Function with all 4 arrays
-let destination = randomizeArrays(destinations)
-let restaurant = randomizeArrays(restaurants)
-let transportation = randomizeArrays(transportations)
-let entertainment = randomizeArrays(entertainments)
+function bucketLogger() {
+    //Call Function with all 4 arrays
+    let destination = randomizeArrays(destinations)
+    let restaurant = randomizeArrays(restaurants)
+    let transportation = randomizeArrays(transportations)
+    let entertainment = randomizeArrays(entertainments)
 
-//Let user know what was randomized
-console.log(destination, restaurant, transportation, entertainment)
-
+    //Let user know what was randomized
+    console.log(`${destination}, ${restaurant}, ${transportation}, ${entertainment}`)
+}
 
 //Prompt user to select complete or to randomize again
-let userInput = prompt('Type "Complete" if your trip is to your liking, otherwise type Try Again.')
-
+let userInput = 'Try Again'
 
 //Create a While Loop for Try Again userInput
 while(userInput == 'Try Again'){
-    destination = randomizeArrays(destinations)
-    restaurant = randomizeArrays(restaurants)
-    transportation = randomizeArrays(transportations)
-    entertainment = randomizeArrays(entertainments)
-    console.log(destination, restaurant, transportation, entertainment)
+    bucketLogger()
     userInput = prompt('Type "Complete" if your trip is to your liking, otherwise type Try Again.')
 }
 
+// while (userInput == 'Try Again') {
+//     destination = randomizeArrays(destinations)
+//     restaurant = randomizeArrays(restaurants)
+//     transportation = randomizeArrays(transportations)
+//     entertainment = randomizeArrays(entertainments)
+//     console.log(destination, restaurant, transportation, entertainment)
+//     userInput = prompt('Type "Complete" if your trip is to your liking, otherwise type Try Again.')
+// }
+
 //Create an if statement for Complete userInput
-if(userInput == 'Complete'){
+if (userInput == 'Complete') {
     console.log('Your trip is booked!')
 }
